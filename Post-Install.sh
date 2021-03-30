3cc2ba84f2f22b51d700f791b0597c84cc162236
xFadedxShadow <63852447+xFadedxShadow@users.noreply.github.com>
2021-03-30 04:42:24 -0400

    Add files via upload

--- Post-Install.sh
+++ Post-Install.sh
@@ -1,1 +1,39 @@
-echo "The Script has ran successfully in chrooted enviorment!"
+#!/usr/bin/env bash
+
+
+## Setting up timezone and Hardware Clock.
+
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
+
+
+## Setting up Locales, Hostname and Hosts.
+
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "null" > /etc/hostname
+echo -e '127.0.0.1       localhost\n::1             localhost\n127.0.1.1       null' > /etc/hosts
+
+
+## Installing System Packages and Configuring bootloader.
+
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog mtools dosfstools linux-zen-headers bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth git reflector xdg-utils xdg-user-dirs
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ARCH
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+## Enabling system services.
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+
+
+## Adding users and Editing sudo
+
+useradd -mG wheel xfadedxshadow
+EDITOR=nano visudo
+
+
+echo "Installation has been completed!"
+exit

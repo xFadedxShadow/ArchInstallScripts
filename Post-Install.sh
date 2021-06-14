9bf1e9fbd150e175910a9c29aa0c5501dafbf479
xFadedxShadow <63852447+xFadedxShadow@users.noreply.github.com>
2021-06-13 23:59:39 -0400

    Add files via upload

--- Post-Install.sh
+++ Post-Install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 
-## Setting up timezone and Hardware Clock.
+## Configuring Timzone and System Clock
 
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc
 
 
-## Setting up Locales, Hostname and Hosts.
+## Generating Locales and Configuring Host Files
 
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
@@ -16,28 +16,29 @@ echo "null" > /etc/hostname
 echo -e '127.0.0.1       localhost\n::1             localhost\n127.0.1.1       null' > /etc/hosts
 
 
-## Installing System Packages and Configuring bootloader.
+## Installing Extra Packages and Configuring GRUB
 
 pacman -S grub efibootmgr networkmanager network-manager-applet dialog mtools dosfstools linux-zen-headers bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth git reflector xdg-utils xdg-user-dirs bash-completion intel-ucode
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ARCH
 grub-mkconfig -o /boot/grub/grub.cfg
 
 
-## Enabling system services.
+## Enabling System Services
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
 
 
-## Adding users and Editing sudo
+## Creating Users and Configuring Groups
 
 useradd -mG wheel xfadedxshadow
 EDITOR=nano visudo
-echo "Enter Root Password!"
+echo "Enter Root Password"
 passwd
 echo "\nEnter User Password!"
 passwd xfadedxshadow
 
 
-echo "Installation has been completed! ( Setup a Graphical Enviorment, Display Server and a Login Manager! )"
-exit
+## Installtion Completed!
+
+echo "Installation is complete!\nSetup a Graphical Enviorment, Display Server and Login Manager of your choice!"
--- Pre-Install.sh
+++ Pre-Install.sh
@@ -1,46 +1,47 @@
 #!/usr/bin/env bash
 
 
-## Synchronizing System Clock. ( Silencing NTP / Causes Weird Issues )
+## Synchronizing System Clock!
 
 timedatectl set-ntp true
 timedatectl set-timezone America/New_York
 hwclock --systohc
 
 
-## Setting up mirrors list.
+## Updating Mirrors
 
 reflector --country 'United States' --sort rate --age 8 --save /etc/pacman.d/mirrorlist
 
 
-## Wiping and Partitioning Disks and Formatting. ( Block Size 16 MB )
+## Wiping Disks and Paritions / Re-Partitioning Disks
 
 dd if=/dev/zero of=/dev/sda bs=16M status=progress && sync
 cfdisk /dev/sda
+
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
 
 
-## Mounting Disks and Creating Directories.
+## Mounting Disks and Paritions
 
 mount /dev/sda2 /mnt
 mkdir -p /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
 
 
-## Installing Base Arch Packages and Generating fstab.
+## Base Installation and Generating fstab
 
 pacstrap /mnt base base-devel linux-zen linux-firmware nano sudo
 genfstab -U /mnt >> /mnt/etc/fstab
 
 
-## Coping files into the new system and running it.
+## Coping files and Executing Post Installation Script
 
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 cp Post-Install.sh /mnt/Post-Install.sh
 arch-chroot /mnt ./Post-Install.sh
 
 
-## Deleting scripts..
+## Removing Installation Script
 
 rm -rf /mnt/Post-Install.sh
